3b8dbdcb697158b3368a8c08480b062a81715c32
Corredato <brunocbotti@gmail.com>
2025-07-01 23:03:55 -0300

    [ADD] initial features: psql, models and templates

--- library/books/models.py
+++ library/books/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-#from ..authors.models import Author
+from authors.models import Author
 
 #TODO: https://isbndb.com/isbndb-api-documentation-v2
 
@@ -10,7 +10,7 @@ class Book(models.Model):
     isbn_13 = models.CharField(max_length=200, default='ISBN 13')
     dewey_decimal = models.CharField(max_length=200, default='Dewey decimal')  # TODO: dict inside list? [{}]
     #binding = models.CharField(max_length=200)
-    publisher = models.CharField(max_length=200, default='Publisher')
+    #publisher = models.CharField(max_length=200, default='Publisher') # TODO: ''.join() strings
     #date_published = models.DateField(null=True) # TODO: maybe many2many like authors field
     edition = models.CharField(max_length=200, default='Edition')
     pages = models.CharField(max_length=200, default='Pages')
@@ -20,6 +20,6 @@ class Book(models.Model):
     excerpt = models.TextField(default='Excerpt')
     msrp = models.IntegerField(default=0)
     synopsis = models.CharField(max_length=200, default='Synopsis')
-    subjects = models.CharField(max_length=200, default='Subjects')  # TODO: ''.join() strings
+    #subjects = models.CharField(max_length=200, default='Subjects')  # TODO: ''.join() strings
     reviews = models.CharField(max_length=200, default='Reviews')  # TODO: ''.join() review strings
-    # authors = models.ManyToManyField(Author) TODO: for author in self ''.join() authors names
+    authors = models.ManyToManyField(Author) #TODO: for author in self ''.join() authors names
